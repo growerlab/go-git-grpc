e5145619d08d857773aaf0c33cee9577a926a28f
moli <molix2@outlook.com>
2021-07-27 18:51:59 +0800

    add test

---
+++ test/init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+baseDir=$GOPATH/src/github.com/growerlab/go-git-grpc/test
+repoDir=$baseDir/testrepo_bare
+tmpRepoDir=/tmp/testrepo_bare
+
+if [ ! -d $repoDir ]; then
+    mkdir -p $repoDir
+    cd $repoDir
+    git init --bare
+fi
+
+
+if [ ! -d $tmpRepoDir ]; then
+    mkdir -p $tmpRepoDir
+    cd $tmpRepoDir
+
+    git init
+    git remote add origin $repoDir
+    for i in {1..10} ; do
+        touch ${i}.txt
+        echo ${i} >> ${i}.txt
+        git add .
+        git commit -m "commit ${i}"
+    done
+
+    for i in {1..5} ; do
+        git checkout -b branch_${i}
+        touch ${i}.log
+        echo ${i} >> ${i}.log
+        git add .
+        git commit -m "commit ${i} in 'branch_${i}'"
+        git tag -a v${i}.0 -m "v${i}.0"
+    done
+
+    git push --all
+    git push --tags
+fi
+
+
---
+++ test/test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"github.com/go-git/go-git/v5/plumbing/object"
+
+	gggrpc "github.com/growerlab/go-git-grpc"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+func main() {
+	go func() {
+		gitRoot := path.Join(os.Getenv("GOPATH"), "src/github.com/growerlab/go-git-grpc")
+		err := gggrpc.NewServer(gitRoot, "localhost:8081")
+		if err != nil {
+			panic(err)
+		}
+	}()
+	time.Sleep(1 * time.Second)
+
+	clientCtx := context.Background()
+	repoPath := "test/testrepo_bare"
+	store, closeFn, err := gggrpc.NewClient(clientCtx, "localhost:8081", repoPath)
+	if err != nil {
+		panic(err)
+	}
+	defer closeFn.Close()
+
+	repo, err := git.Open(store, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	testReferences(repo)
+	testTags(repo)
+	testCommits(repo)
+	// tag下的文件列表
+	testFileTreesInTag(repo, "v1.0")
+}
+
+func testFileTreesInTag(repo *git.Repository, tagName string) {
+	tag, err := repo.Tag(tagName)
+	if err != nil {
+		log.Fatalf("get tag '%s' was err: %+v", tagName, err)
+	}
+	tree, err := repo.TreeObject(tag.Hash())
+	if err != nil {
+		log.Fatalf("get tree '%s' was err: %+v", tagName, err)
+	}
+	n := 0
+	tree.Files().ForEach(func(file *object.File) error {
+		n++
+		log.Printf("file %s in tag '%s'", file.Name, tagName)
+		return nil
+	})
+}
+
+func testCommits(repo *git.Repository) {
+	commitIter, err := repo.CommitObjects()
+	if err != nil {
+		log.Fatalf("get commits was err: %+v", err)
+	}
+	n := 0
+	_ = commitIter.ForEach(func(c *object.Commit) error {
+		n++
+		log.Printf("commit committer: %s message: %s hash: %s\n",
+			c.Committer.String(),
+			c.Message,
+			c.Hash.String(),
+		)
+		fileIter, err := c.Files()
+		if err != nil {
+			log.Fatalf("get files in %s was err: %+v", c.Hash.String(), err)
+		}
+		fileIter.ForEach(func(file *object.File) error {
+			cts, err := file.Contents()
+			if err != nil {
+				log.Fatalf("get file %s in %s was err: %+v", file.Name, c.Hash.String(), err)
+			}
+			log.Println("file '%s' content: %+v\n", file.Name, cts)
+			return nil
+		})
+		return nil
+	})
+}
+
+func testTags(repo *git.Repository) {
+	refs, err := repo.Tags()
+	if err != nil {
+		log.Fatalf("get tags was err: %+v", err)
+	}
+	n := 0
+	_ = refs.ForEach(func(r *plumbing.Reference) error {
+		n++
+		log.Printf("tag name: %s type: %d hash: %s string: %s target: %s\n",
+			r.Name(),
+			r.Type(),
+			r.Hash().String(),
+			r.String(),
+			r.Target())
+		return nil
+	})
+	if n == 0 {
+		log.Fatalf("Not found tags")
+	}
+}
+
+func testReferences(repo *git.Repository) {
+	refs, err := repo.References()
+	if err != nil {
+		log.Fatalf("get branchs was err: %+v", err)
+	}
+	n := 0
+	_ = refs.ForEach(func(r *plumbing.Reference) error {
+		n++
+		log.Printf("branch name: %s type: %d hash: %s string: %s target: %s\n",
+			r.Name(),
+			r.Type(),
+			r.Hash().String(),
+			r.String(),
+			r.Target())
+		return nil
+	})
+	if n == 0 {
+		log.Fatalf("Not found branchs")
+	}
+}

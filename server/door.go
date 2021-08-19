d144ac769d98e70fd15fb02408cb7b23eaa4d159
moli <molix2@outlook.com>
2021-08-19 21:10:55 +0800

    stash

--- server/door.go
+++ server/door.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/pkg/errors"
+
 	"github.com/go-git/go-git/v5/plumbing/protocol/packp"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/go-git/go-git/v5/plumbing/transport/server"
 	"github.com/go-git/go-git/v5/utils/ioutil"
 	"github.com/growerlab/go-git-grpc/pb"
-	"github.com/reactivex/rxgo/v2"
 )
 
 // ServerCommand is used for a single server command execution.
@@ -18,6 +19,40 @@ type ServerCommand struct {
 	Stderr io.Writer
 	Stdout io.WriteCloser
 	Stdin  io.Reader
+
+	repoPath   string
+	uploadPack pb.Door_ServeUploadPackServer
+}
+
+// Start 协议：第一个请求仅包含仓库路径，不传数据
+func (s *ServerCommand) Start() error {
+	firstReq, err := s.uploadPack.Recv()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	s.repoPath = firstReq.Path
+	s.Stdout = s
+	s.Stderr = s
+	s.Stdin = s
+
+	return nil
+}
+
+func (s *ServerCommand) Read(p []byte) (n int, err error) {
+	req, err := s.uploadPack.Recv()
+	if err != nil {
+		return len(req.Raw), errors.WithStack(err)
+	}
+	return copy(p, req.Raw), nil
+}
+
+func (s *ServerCommand) Write(p []byte) (n int, err error) {
+	err = s.uploadPack.Send(&pb.Response{Raw: p})
+	return len(p), errors.WithStack(err)
+}
+
+func (s *ServerCommand) Close() error {
+	return nil
 }
 
 var _ pb.DoorServer = (*Door)(nil)
@@ -26,21 +61,14 @@ type Door struct {
 }
 
 func (d *Door) ServeUploadPack(pack pb.Door_ServeUploadPackServer) error {
-	ep, err := transport.NewEndpoint(path)
-	if err != nil {
+	srvCmd := ServerCommand{uploadPack: pack}
+	if err := srvCmd.Start(); err != nil {
 		return err
 	}
 
-	firstPack := pack.Recv()
-
-	rxgo.FromEventSource()
-
-	reader, writer := io.Pipe()
-
-	srvCmd := ServerCommand{
-		Stderr: nil,
-		Stdout: nil,
-		Stdin:  nil,
+	ep, err := transport.NewEndpoint(srvCmd.repoPath)
+	if err != nil {
+		return err
 	}
 
 	// 在服务端不检测授权，由客户端做
@@ -53,7 +81,22 @@ func (d *Door) ServeUploadPack(pack pb.Door_ServeUploadPackServer) error {
 }
 
 func (d *Door) ServeReceivePack(pack pb.Door_ServeReceivePackServer) error {
+	srvCmd := ServerCommand{uploadPack: pack}
+	if err := srvCmd.Start(); err != nil {
+		return err
+	}
 
+	ep, err := transport.NewEndpoint(srvCmd.repoPath)
+	if err != nil {
+		return err
+	}
+
+	s, err := server.DefaultServer.NewReceivePackSession(ep, nil)
+	if err != nil {
+		return fmt.Errorf("error creating session: %s", err)
+	}
+
+	return d.serveReceivePack(srvCmd, s)
 }
 
 func (d *Door) serveUploadPack(cmd ServerCommand, s transport.UploadPackSession) (err error) {
